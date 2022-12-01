dd39a734af07dac64b58ede1f456e65a8706f9b7
Kamil <k.medrala99@gmail.com>
2022-12-01 21:58:27 +0100

    pinia init with register functions

--- pages/register.vue
+++ pages/register.vue
@@ -3,19 +3,35 @@
         <div class="p-5 bg-white">
             <h1 class="text-4xl mb-5"> Register </h1>
             <form method="post" @submit.prevent="register" class="w-full max-w-md">
-                <input type="text" name="username" id="username" placeholder="User name" class="input-field">
-                <input type="text" name="password" id="password" placeholder="Password" class="input-field">
+                <input v-model="username" type="text" name="username" id="username" placeholder="User name" class="input-field">
+                <input v-model="password" type="text" name="password" id="password" placeholder="Password" class="input-field">
                 <button class="button-default"> Register </button>
             </form>
+            <p v-if="result"> {{result}}</p>
         </div>
     </div>
 </template>
 
 <script>
+import { mainStore } from '~/store/index'
 export default {
+    data(){
+        return{
+            username:'',
+            password:''
+        }
+    },
+    computed:{
+        result(){
+            return mainStore().getRegisterRes
+        }
+    },
     methods:{
         register(){
-            console.log('register form sent');
+            if (this.username && this.password) {
+                console.log({username:this.username, password:this.password, confirmedPassword:this.password});
+                mainStore().register({username:this.username, password:this.password, confirmedPassword:this.password})
+            }
         }
     }
 }
